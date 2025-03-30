7c69604723fe991e40756273a6a123e2c26bf30c
ink19 <ink19@qq.com>
2025-03-30 22:00:12 +0800

    添加rest api

--- market/okx/rest_api.h
+++ market/okx/rest_api.h
@@ -1,6 +1,17 @@
 #ifndef __MARKER_OKX_REST_API_H__
 #define __MARKER_OKX_REST_API_H__
 
+namespace Market {
 
+namespace Okx {
+
+class RestApi {
+public:
+  
+};
+
+} // namespace Okx
+
+} // namespace Market
 
 #endif
